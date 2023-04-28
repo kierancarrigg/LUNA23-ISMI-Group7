7df8af37dec90266c544c0ca92d4ba8d2b3524c6
Serah Sommers <ssommers@int4.local.snellius.surf.nl>
2023-04-28 15:09:43 +0200

    experimented with job submitting

--- batch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Set job requirements
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --gpus=1
-#SBATCH --partition=gpu
---
+++ experiment-job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Set job requirements
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --time=01:00:00
+
+# Load modules
+module load 2021
+module load Python/3.9.5-GCCcore-10.3.0
+
+# Execute program
+python python_test.py
